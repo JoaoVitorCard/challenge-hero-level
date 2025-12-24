0bc9425f55b0c38ad1b5411e945aca548833233b
João Vitor <joaovitornascim@gmail.com>
2025-12-24 15:09:53 -0300

    Removed the unsual file

--- node_5e1221577045f.tmp.js
+++
@@ -1,6 +0,0 @@
-const prompt = require("prompt-sync")();
-
-const heroName = prompt("Nome do Herói: ");
-const heroXP = Number(prompt("Qual a quantidade de XP do Herói: "));
-
-console.log(heroName, heroXP);
